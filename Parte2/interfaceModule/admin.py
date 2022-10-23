6f02802e31325f480d3480b9555729465cb0c31f
Celtan15 <dagutierrezv@uqvirtual.edu.co>
2022-10-23 17:30:49 -0500

    Se hacen diversas modificaciones, se termina la fase de aprendizaje del framework y comienza el desarrollo de la apk

--- Parte2/interfaceModule/admin.py
+++ Parte2/interfaceModule/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from interfaceModule.models import InterfaceModule
+from interfaceModule.models import InterfaceEvaluation
+from interfaceModule.models import Person
 
-admin.site.register(InterfaceModule)
+class UserAdmin(admin.ModelAdmin):
+    list_display=('id', 'name', 'nickname', 'email', 'country', 'date_birth')
+    search_fields=('id', 'name', 'nickname', 'email')
+    list_filter=('name', 'date_birth', 'country')
+
+class ModuleAdmin(admin.ModelAdmin):
+    list_display=('name', 'status', 'moduleLocked')
+    search_fields=('id', 'name', 'status')
+    list_filter=('status', 'progression')
+
+class ModuleEvaluation(admin.ModelAdmin):
+    list_display=('name', 'id', 'questions')
+    search_fields=('id', 'name', 'qualification')
+    list_filter=('qualification', 'name')
+
+admin.site.register(Person, UserAdmin)
+admin.site.register(InterfaceModule, ModuleAdmin)
+admin.site.register(InterfaceEvaluation, ModuleEvaluation)
 # Register your models here.
--- Parte2/interfaceModule/models.py
+++ Parte2/interfaceModule/models.py
@@ -18,24 +18,36 @@ class Person(models.Model):
     email=models.EmailField()
     psdw=models.CharField(PasswordInput, max_length=15)
     country=models.CharField(max_length=30)
-    dateBirth=models.DateField()
+    date_birth=models.DateField()
     phone=models.CharField(max_length=20, blank=True)
 
-class  InterfaceEvaluation(models.Model):
+    def __str__(self):
+        return 'El usuario se llama %s , su nickname es %s , su correo es %s y reside en %s' %(self.name,
+        self.nickname, self.email, self.country)
+
+class InterfaceEvaluation(models.Model):
     name=models.CharField(max_length=50)
     answer=models.CharField(max_length=5, blank=True)
-    question=models.CharField(max_length=5, blank=True)
+    questions=models.CharField(max_length=5, blank=True)
     qualification=models.DecimalField(max_digits=5, decimal_places=3, blank=True)
+    
+    def __str__(self):
+        return 'El modulo se llama %s , posee las siguientes preguntas %s y evalua los conocimientos en interfaz de FL' %(self.name,
+        self.questions)
 
 class User(Person):
-    totalProgresion=DecimalField()
+    total_progression=DecimalField()
 
 class Admin(Person):
-    addedUsers=IntegerField()
+    added_users=IntegerField()
     
 class InterfaceModule(models.Model):
     name=models.CharField(max_length=50)
     moduleLocked=models.BooleanField(True)
-    state=models.BooleanField(blank=False)
+    status=models.BooleanField(blank=False)
     progression=models.DecimalField(max_digits=4, decimal_places=2)
-    quantityMicroModules=models.IntegerField(blank=True)
+    quantity_microModules=models.IntegerField(blank=True)
+
+    def __str__(self):
+        return 'El modulo se llama %s , se encuentra %s y posee %s micromódulos' %(self.name, self.status, 
+        self.quantity_microModules)
